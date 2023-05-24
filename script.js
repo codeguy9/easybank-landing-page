12d95dddb822e325d35fed2b38897b0e3b528887
Pavel Bogdan <bogdanpavel500@gmail.com>
2023-05-24 14:03:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuButton = document.querySelector('.menu-icon-toggler');
+const donwnMenu = document.querySelector('.downmenu');
+
+
+menuButton.addEventListener('click', () => {
+    
+        donwnMenu.classList.toggle('toggle');
+    
+    
+})
+
